9b196fb9751846b2ae8c9dd93d627ddf3b467e36
Danny <lunde.andrews@gmail.com>
2022-04-27 13:24:05 -0400

    Add testing

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
 
-const pool = new Pool({
-  database: "tasktime",
-});
+const createServer = (pool) => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/tasks", (req, res) => {
-  pool.query("SELECT * FROM tasks").then((result) => {
-    res.send(result.rows);
+  app.get("/api/tasks", (req, res) => {
+    pool.query("SELECT * FROM tasks").then((result) => {
+      res.send(result.rows);
+    });
   });
-});
 
-app.delete("/api/tasks/:id", async (req, res) => {
-  const { id } = req.params;
+  app.delete("/api/tasks/:id", async (req, res) => {
+    const { id } = req.params;
 
-  await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
-  res.sendStatus(200);
-});
+    await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+    res.sendStatus(200);
+  });
 
-app.patch("/api/tasks/:id", async (req, res) => {
-  const { id } = req.params;
-  const { description } = req.body;
-  const result = await pool.query(
-    "UPDATE tasks SET description=$1 WHERE id=$2 RETURNING *",
-    [description, id]
-  );
-  res.send(result.rows[0]);
-});
+  app.patch("/api/tasks/:id", async (req, res) => {
+    const { id } = req.params;
+    const { description } = req.body;
+    const result = await pool.query(
+      "UPDATE tasks SET description=$1 WHERE id=$2 RETURNING *",
+      [description, id]
+    );
+    res.send(result.rows[0]);
+  });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
+  return app;
+};
+
+module.exports = createServer;
---
+++ server.test.js
@@ -0,0 +1,22 @@
+const createServer = require("./server");
+const supertest = require("supertest");
+const { Pool } = require("pg");
+
+let pool = new Pool({
+  database: "test",
+});
+
+const server = createServer(pool);
+
+afterAll(() => {
+  pool.end();
+});
+
+test("GET /api/tasks", async () => {
+  const response = await supertest(server).get("/api/tasks").expect(200);
+  expect(response.body).toEqual([]);
+});
+
+test("DELETE /api/tasks", async () => {
+  await supertest(server).delete("/api/tasks/3").expect(200);
+});
