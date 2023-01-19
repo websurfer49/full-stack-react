d2181ca734e2cda6151cc274091db5cc4d26af94
Danny <lunde.andrews@gmail.com>
2023-01-19 12:06:43 -0500

    Update best cohort

--- client/index.jsx
+++ client/index.jsx
@@ -4,6 +4,7 @@ import App from "./components/App.jsx";
 import { RecoilRoot } from "recoil";
 
 const root = createRoot(document.getElementById("root"));
+
 root.render(
   <RecoilRoot>
     <App />
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const pool = new pg.Pool({
 });
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
